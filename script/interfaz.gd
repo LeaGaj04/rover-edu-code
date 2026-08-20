e6cbbc2587d4126d103677093ebd7c28cee9d805
zGhosthXzed <nicolasrojasmillar5922@gmail.com>
2026-08-20 19:28:55 -0400

    Cambios de logica de movimiento de rover (Puntos Cardinales)

--- script/interfaz.gd
+++ script/interfaz.gd
@@ -6,20 +6,20 @@ extends CanvasLayer
 
 func _on_button_pressed() -> void:
 	# 1. Obtenemos el texto del usuario
-	var texto_usuario = $TextEdit.text 
+	var texto_usuario = caja_codigo.text
 	
 	# 2. El Lexer limpia y separa el texto
 	var mi_lexer = Lexer.new()
 	var tokens = mi_lexer.tokenizar(texto_usuario)
 	
-	# 3. EL PARSER: Verificamos si la estructura tiene sentido
+	# 3. EL PARSER: Verificamos si la estructura tiene sentido (ej: rover.norte())
 	if tokens.size() == 5:
 		var es_objeto_rover = tokens[0].valor == "rover"
 		var tiene_punto = tokens[1].tipo == Lexer.TipoToken.PUNTO
 		var tiene_parentesis = tokens[3].tipo == Lexer.TipoToken.PARENTESIS_IZQ and tokens[4].tipo == Lexer.TipoToken.PARENTESIS_DER
 		
 		if es_objeto_rover and tiene_punto and tiene_parentesis:
-			var comando = tokens[2].valor 
+			var comando = tokens[2].valor
 			
 			# 4. EL INTÉRPRETE: Movemos el rover
 			ejecutar_movimiento_rover(comando)
@@ -36,17 +36,21 @@ func ejecutar_movimiento_rover(comando: String):
 		print("Error: El Rover no está asignado en el Inspector.")
 		return
 		
-	if comando == "avanzar":
-		print("El rover avanza")
-		mi_rover.avanzar()  # <-- LE DAMOS LA ORDEN REAL AL NODO 3D
+	if comando == "norte":
+		print("El rover se mueve hacia el Norte")
+		mi_rover.norte()
 		
-	elif comando == "izquierda":
-		print("El rover gira a la izquierda")
-		mi_rover.izquierda() # <-- ASEGÚRATE QUE LA FUNCIÓN EN EL ROVER SE LLAME ASÍ
+	elif comando == "sur":
+		print("El rover se mueve hacia el Sur")
+		mi_rover.sur()
 		
-	elif comando == "derecha":
-		print("El rover gira a la derecha")
-		mi_rover.derecha()   # <-- ASEGÚRATE QUE LA FUNCIÓN EN EL ROVER SE LLAME ASÍ
+	elif comando == "este":
+		print("El rover se mueve hacia el Este")
+		mi_rover.este()
+		
+	elif comando == "oeste":
+		print("El rover se mueve hacia el Oeste")
+		mi_rover.oeste()
 		
 	else:
 		print("Error A.D.A: El rover no conoce el comando '", comando, "'")
